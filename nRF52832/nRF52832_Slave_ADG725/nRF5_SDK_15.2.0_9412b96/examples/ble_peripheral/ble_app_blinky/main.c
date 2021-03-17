394ac78499ca63aac6b45ac980c5ed7dc9ca106d
Atomie CHEN <atomic_cwh@163.com>
2021-03-17 18:33:40 +0800

    Fix：ADG725非DFU版本main.c中的锟斤拷乱码

--- nRF52832/nRF52832_Slave_ADG725/nRF5_SDK_15.2.0_9412b96/examples/ble_peripheral/ble_app_blinky/main.c
+++ nRF52832/nRF52832_Slave_ADG725/nRF5_SDK_15.2.0_9412b96/examples/ble_peripheral/ble_app_blinky/main.c
@@ -620,7 +620,7 @@ void saadc_init_quick_sample(void)
 #define CSB 5
 static void thi_monitor_handler(void)
 {
-	// ����Ѿ������ϣ����ϴ����ݵ�����������ִ�к���Ĳ���
+	// 如果已经连接上，则上传数据到主机，否则不执行后面的操作
 	if(m_conn_handle == BLE_CONN_HANDLE_INVALID) {
 		return;
 	}
@@ -650,7 +650,7 @@ static void thi_monitor_handler(void)
 			reg_x++;
 		}
 		
-		//�ض�DataRead
+		//截短DataRead
 		for(int i=0;i<256;i+=8){
 			for(int j=0;j<7;j++){
 				uint8_t now=DataRead[i+j],next = DataRead[i+j+1];
@@ -673,7 +673,7 @@ static void thi_monitor_handler(void)
 			}
 }
 
-//��ʱ����ʱ�жϲ���
+//定时器超时中断操作
 static void thi_monitor_timeout_handler(nrf_timer_event_t event_type,void * p_context)
 {
 	    switch (event_type)
@@ -698,7 +698,7 @@ static void timers_init1(void)
     // Initialize timer module, making it use the scheduler
 		// APP_TIMER_INIT(APP_TIMER_PRESCALER, APP_TIMER_OP_QUEUE_SIZE, false);
 	
-	  // ����һ������ִ�еĶ�ʱ�������ڴ���AD
+	  // 创建一个周期执行的定时器，用于触发AD
     nrf_drv_timer_config_t timer_cfg = NRF_DRV_TIMER_DEFAULT_CONFIG;
     timer_cfg.bit_width = NRF_TIMER_BIT_WIDTH_32;
     err_code = nrf_drv_timer_init(&m_timer, &timer_cfg, thi_monitor_timeout_handler);
@@ -706,7 +706,7 @@ static void timers_init1(void)
 }
 
 
-//��ʱ����ʼ��ʱ��ʱ���� __ ms
+//定时器开始计时，时间间隔 __ ms
 /**@brief Function for starting application timers.
  */
 static void application_timers_start(void)
@@ -747,7 +747,7 @@ void saadc_sampling_event_init(void)
     APP_ERROR_CHECK(err_code);
 
     /* setup m_timer for compare event every __ ms/us */
-    // ���ö�ʱ������/�Ƚ�ͨ�����Ƚ�ֵ������Ƚ����񣬹ر�ʱ���ж�
+    // 设置定时，捕获/比较通道，比较值，清除比较任务，关闭时钟中断
     uint32_t ticks = nrf_drv_timer_us_to_ticks(&m_timer, 25);
     nrf_drv_timer_extended_compare(&m_timer,
                                    NRF_TIMER_CC_CHANNEL2,
@@ -756,7 +756,7 @@ void saadc_sampling_event_init(void)
                                    false);
     nrf_drv_timer_enable(&m_timer);
 
-    // ����PPI���˵�ͨ����һ���ǱȽ��¼���һ����adc�ɼ�����
+    // 设置PPI两端的通道，一个是比较事件，一个是adc采集任务
     uint32_t timer_compare_event_addr = nrf_drv_timer_compare_event_address_get(&m_timer,
                                                                                 NRF_TIMER_CC_CHANNEL2);
     uint32_t saadc_sample_task_addr   = nrf_drv_saadc_sample_task_get();
@@ -774,7 +774,7 @@ void saadc_sampling_event_init(void)
 
 void saadc_sampling_event_enable(void)
 {
-    // ʹ��PPIͨ��
+    // 使能PPI通道
     ret_code_t err_code = nrf_drv_ppi_channel_enable(m_ppi_channel);
 
     APP_ERROR_CHECK(err_code);
@@ -795,7 +795,7 @@ void loop_init(void) {
 }
 
 void send_data(void) {
-	//�ض�DataRead
+	//截短DataRead
 	for(int i=0;i<256;i+=8){
 		for(int j=0;j<7;j++){
 			uint8_t now=DataRead[i+j],next = DataRead[i+j+1];
@@ -803,7 +803,7 @@ void send_data(void) {
 		}
 	}
 
-	// �ϴ����ݵ�����
+	// 上传数据到主机
 	ble_lbs_on_button_change1(m_conn_handle, &m_lbs, DataRead_short);
 	
 	// add by cwh
@@ -817,23 +817,23 @@ void send_data(void) {
 
 void saadc_callback2(nrf_drv_saadc_evt_t const * p_event)
 {
-    // adc�ɼ��жϻص�����
+    // adc采集中断回调函数
 
     if (p_event->type == NRF_DRV_SAADC_EVT_DONE)
     {
         ret_code_t err_code;
 
-				// Ϊ�´β���׼���û���
+				// 为下次采样准备好缓存
         err_code = nrf_drv_saadc_buffer_convert(p_event->data.done.p_buffer, SAMPLES_IN_BUFFER);
         APP_ERROR_CHECK(err_code);
 
-        // δ����������������ִ��
+        // 未连接蓝牙，则不向下执行
         if(m_conn_handle == BLE_CONN_HANDLE_INVALID) {
             loop_init();
             return;
         }
 
-				// ��ȡadc����ת��ֵ
+				// 读取adc采样转换值
         nrf_saadc_value_t saadc_val = p_event->data.done.p_buffer[0];
 				if (saadc_val < 0) {
 					DataRead[16*i+k] = 0;
@@ -890,11 +890,11 @@ void saadc_init_ppi(void)
     err_code = nrf_drv_saadc_channel_init(0, &channel_config);
     APP_ERROR_CHECK(err_code);
 
-    // ����һ������
+    // 配置一个缓冲
     err_code = nrf_drv_saadc_buffer_convert(m_buffer_pool[0], SAMPLES_IN_BUFFER);
     APP_ERROR_CHECK(err_code);
 
-		// ���õڶ�������
+		// 配置第二个缓冲
     // err_code = nrf_drv_saadc_buffer_convert(m_buffer_pool[1], SAMPLES_IN_BUFFER);
     // APP_ERROR_CHECK(err_code);
 
@@ -908,11 +908,11 @@ void saadc_callback_out(nrf_drv_saadc_evt_t const * p_event)
         // NRF_LOG_INFO("## saadc int start");
         ret_code_t err_code;
 
-        // Ϊ�´β���׼���û���
+        // 为下次采样准备好缓存
         err_code = nrf_drv_saadc_buffer_convert(p_event->data.done.p_buffer, SAMPLES_IN_BUFFER);
         APP_ERROR_CHECK(err_code);
 
-        // ��ȡadc����ת��ֵ
+        // 读取adc采样转换值
         out_voltage = p_event->data.done.p_buffer[0];
         cnt += 1;
 
@@ -934,7 +934,7 @@ void saadc_init_out(void)
     err_code = nrf_drv_saadc_channel_init(0, &channel_config);
     APP_ERROR_CHECK(err_code);
 
-    // ����һ������
+    // 配置一个缓冲
     err_code = nrf_drv_saadc_buffer_convert(m_buffer_pool[0], SAMPLES_IN_BUFFER);
     APP_ERROR_CHECK(err_code);
 
