5aad840739b6eba5bf09b2ddb4f1167d1a1ea925
Atomie CHEN <atomic_cwh@163.com>
2021-03-20 21:11:16 +0800

    Fix：主机代码串口失效，将pca10040.h板子对串口引脚的定义恢复为官方SDK中的原始编号
    之前对串口引脚的修改来自于从机代码（可能与SPI引脚冲突），但现在从机不需要串口，故可以改回

--- nRF5_SDK_15.2.0_9412b96/components/boards/pca10040.h
+++ nRF5_SDK_15.2.0_9412b96/components/boards/pca10040.h
@@ -86,10 +86,10 @@ extern "C" {
 #define BSP_BUTTON_2   BUTTON_3
 #define BSP_BUTTON_3   BUTTON_4
 
-#define RX_PIN_NUMBER  5//8
-#define TX_PIN_NUMBER  10
+#define RX_PIN_NUMBER  8
+#define TX_PIN_NUMBER  6
 #define CTS_PIN_NUMBER 7
-#define RTS_PIN_NUMBER 11//5
+#define RTS_PIN_NUMBER 5
 #define HWFC           true
 
 #define SPIS_MISO_PIN   28  // SPI MISO signal.
